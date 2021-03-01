ec2e80917c320a56034e5f79dde499c874294806
Elise <elisztomania@gmail.com>
2021-03-01 16:31:08 +0100

    config + change location adress to coordonates

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,11 +1,13 @@
 class Spot < ApplicationRecord
   has_many_attached :photos
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 
-  CAT = ["Café", "Vue", "Parc", "Archi", "Shop", "Food", "Fripe", "Ecolo"]
+  CAT = ["café", "vue", "parc", "archi", "shop", "food", "fripe", "ecolo"]
 
   validates :name, presence: true
   validates :location, presence: true
   validates :category, presence: true, inclusion: { in: CAT }
   validates :description, presence: true
-  validates :photos, presence: true
+  # validates :photos, presence: true
 end
